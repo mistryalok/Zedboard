06acbf3bbf82fc02743c5c7cf801eb1124dfebdd
mistryalok <alokn73@gmail.com>
2016-04-02 22:33:48 +0530

    S09 last vd

---
+++ learning/training/MSD/s09/axi_dma_sg/vivado/project_1/project_1.sdk/fsbl/Debug/src/ps7_init.d
@@ -0,0 +1,29 @@
+src/ps7_init.o src/ps7_init.d: \
+ /home/alok/s09/axi_dma_sg/vivado/project_1/project_1.sdk/design_1_wrapper_hw_platform_0/ps7_init.c \
+ /home/alok/s09/axi_dma_sg/vivado/project_1/project_1.sdk/design_1_wrapper_hw_platform_0/ps7_init.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+/home/alok/s09/axi_dma_sg/vivado/project_1/project_1.sdk/design_1_wrapper_hw_platform_0/ps7_init.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../fsbl_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
