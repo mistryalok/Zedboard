0d2ee2f5555ac94008651169006bfe1031f8f7fa
mistryalok <alokn73@gmail.com>
2016-04-03 17:22:35 +0530

    Microsys labs

---
+++ learning/training/Microsystem/axi_interface_part2/project_1/project_1.sdk/hello/Debug/src/helloworld.d
@@ -0,0 +1,8 @@
+src/helloworld.o src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h ../src/ps7_init.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../src/ps7_init.h:
